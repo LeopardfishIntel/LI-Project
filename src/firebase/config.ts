21b0370ad5264ff622531ce891c546361aa08eb6
Roger Keen <roger@leopardfish.com>
2026-02-15 08:24:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2840117705-12faa",
+  "appId": "1:342003687950:web:fd1a0fc72ace91cf1c125f",
+  "apiKey": "AIzaSyCsXjVXsRxZerNaYj7kFWTyxdMlR6kLK9U",
+  "authDomain": "studio-2840117705-12faa.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "342003687950"
+};
