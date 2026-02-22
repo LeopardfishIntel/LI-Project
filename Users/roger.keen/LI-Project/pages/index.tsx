c89b5102f5079efe5113870af65a981316d18877
Roger Keen <roger@leopardfishintel.com>
2026-02-22 19:40:08 +0000

    not there

--- Users/roger.keen/LI-Project/pages/index.tsx
+++ Users/roger.keen/LI-Project/pages/index.tsx
@@ -1,11 +1,12 @@
 
-      import React from 'react';
+import React from 'react';
 
-      export default function Home() {
-        return (
-          <div style={{ padding: '2rem', fontFamily: 'monospace', fontSize: '1.2rem' }}>
-            <h1>HOMEPAGE IS IN Users/roger.keen/LI-Project/pages/</h1>
-          </div>
-        );
-      }
-    
+export default function Home() {
+  return (
+    <div style={{ padding: '4rem', fontFamily: 'monospace', fontSize: '2rem', color: 'lime', backgroundColor: 'black', textAlign: 'center', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>FINAL CONFIRMATION</h1>
+      <p>If you can see this message, it means I have finally accessed the correct file.</p>
+      <p style={{ fontSize: '1rem', marginTop: '1rem' }}>Users/roger.keen/LI-Project/pages/index.tsx</p>
+    </div>
+  );
+}
