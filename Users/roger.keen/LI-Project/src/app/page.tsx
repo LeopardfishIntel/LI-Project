15fcd5d177d91705554374228d186f18ba676c2b
Roger Keen <roger@leopardfishintel.com>
2026-02-28 07:05:48 +0000

    tell me the code change as the error has not gone

--- Users/roger.keen/LI-Project/src/app/page.tsx
+++ Users/roger.keen/LI-Project/src/app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
         </div>
       </section>
 
-      {/* Tactical Tagline Section - consolidated and refined */}
+      {/* Tactical Tagline Section - Consolidated and Refined */}
       <section className="py-12 bg-background border-y border-white/5">
         <div className="container mx-auto px-4 md:px-6 text-center">
             <p className="text-[10px] sm:text-xs font-black text-white tracking-[0.4em] uppercase opacity-100">
