d58d103cc075683f291eb6da5a61cc5c61bfdccd
LeopardfishIntel <roger@leopardfishintel.com>
2026-09-13 09:27:49 +0200

    Increase Vietnam public transit cost by 50% to 525,000 VND per month

--- src/app/financial-forecaster/page.tsx
+++ src/app/financial-forecaster/page.tsx
@@ -1385,7 +1385,7 @@ const historicMonths = useMemo(() => {
         const argSingleUsd = (130000 / (currentRates['ARS'] || 1200)) * (currentRates['USD'] || 1.27);
         transportVal = argSingleUsd * (transitScalarMap[transportKey] || 1.0);
       } else if (sCountry === 'vietnam') {
-        const vnSingleUsd = (350000 / (currentRates['VND'] || 31614)) * (currentRates['USD'] || 1.27);
+        const vnSingleUsd = (525000 / (currentRates['VND'] || 31614)) * (currentRates['USD'] || 1.27);
         transportVal = vnSingleUsd * (transitScalarMap[transportKey] || 1.0);
       } else if (typeof transportMap === 'object' && transportMap !== null) {
         if (transportMap[transportKey] !== undefined && safeParse(transportMap[transportKey]) > 0) {
